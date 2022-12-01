505be704e5be06ec4b262a99fef06f7305ee6b4b
LeaoSoriano <94892692+LeaoSoriano@users.noreply.github.com>
2022-11-30 18:13:59 -0600

    Update config.php

--- sicvi/php/config.php
+++ sicvi/php/config.php
@@ -3,7 +3,6 @@
         'host' => '185.27.134.10',
         'username' => 'repiz_33095795',
         'password' => 'gyZVi7Jb2BJjbp',
-        'db' => 'epiz_33095795_sicvi',
-        'port' => '3306'
+        'db' => 'epiz_33095795_sicvi'
     ];
 ?>
